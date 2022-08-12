96c117369dde6e441ea21ef86c0c46ddcc2880be
MrCheng <496177750@qq.com>
2022-08-12 22:49:45 +0800

    push测试

--- src/main/java/com/xiaomi/gittest/GitTest.java
+++ src/main/java/com/xiaomi/gittest/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println();
         System.out.println();
         System.out.println("hot-fix-test");
+        System.out.println("hot");
+        System.out.println("test");
     }
 }
